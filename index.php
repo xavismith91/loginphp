4ac664d2ad734239bfe94ba5e042bb9abe4082cc
unknown <xaviersolano91@gmail.com>
2023-01-19 09:52:27 -0600

    190123-login-9:52

--- index.php
+++ index.php
@@ -4,8 +4,20 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
 </head>
+<body>
+
+    <form action="validar.php" method="POST">
+    <h1>Login</h1>
+    <p>Usuario<input type="text" placeholder="Ingresa tu nombre" name="usuario"></p>
+    <p>Constraseña <input type="text" placeholder="Ingresa tu contraseña " name="contraseña"> </p>
+    <input type="submint" value="ingreso">
+
+
+    </form>
+</body>
 
 
 
